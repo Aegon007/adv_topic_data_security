2d3a81b4834d39faba529426a0783dc66f8d0517
Carl007 <33367672+Aegon007@users.noreply.github.com>
2020-04-24 13:47:42 -0400

    show pic

--- network_analysis/README.m
+++ network_analysis/README.m
@@ -37,7 +37,7 @@ have troubel to understand the help msg:
 
 
 Below is an running example to show how it looks like when the code is running:
-![image](https://github.com/Aegon007/adv_topic_data_security/blob/master/network_analysis/screen_shot.png)
+![image](https://raw.githubusercontent.com/Aegon007/adv_topic_data_security/master/network_analysis/screen_shot.png)
 
 ** Note
 1. Feel free to modify the code:)
